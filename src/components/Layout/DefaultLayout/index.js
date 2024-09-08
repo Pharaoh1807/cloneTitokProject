769bfaa206e695e5663b16f8576d1b763c0d9fb9
Pharaoh <thanhhao056mc2014@gmail.com>
2024-09-08 21:41:59 +0700

    Dung khung Default Layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,12 +1,17 @@
-import Header from './Header';
+import styles from './DefaultLayout.module.scss';
+import Header from '../components/Header';
 import Sidebar from './Sidebar';
+import clasNames from 'classnames/bind';
+
+const cx = clasNames.bind(styles);
+
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="comtainer">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
